d0658c510f15be08e9c73597f5c987e0a754225b
Lorenelle <lorencisneros00@gmail.com>
2022-06-03 15:32:33 +0800

    adding public user in Domain Model

--- QualifyMeProject.DomainModels/User.cs
+++ QualifyMeProject.DomainModels/User.cs
@@ -5,7 +5,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace QualifyMeProject.DomainModels
 {
-    internal class User
+    public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+        public string Mobile { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsCompany { get; set; }
+
     }
 }
