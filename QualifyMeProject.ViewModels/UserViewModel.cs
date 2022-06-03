c484cd464a74de02d6c271d4e7e4cf08f8a5089e
Louela <louelabalili15@gmail.com>
2022-06-03 16:59:09 +0800

    Update all and Adding UserViewModel

--- QualifyMeProject.ViewModels/UserViewModel.cs
+++ QualifyMeProject.ViewModels/UserViewModel.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace QualifyMeProject.DomainModels
+namespace QualifyMeProject.ViewModels
 {
-    public class User 
+    public class UserViewModel
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserID { get; set; }
         public int ID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        public string Password { get; set; }
         public string Mobile { get; set; }
         public bool IsAdmin { get; set; }
         public bool IsCompany { get; set; }
-
     }
 }
