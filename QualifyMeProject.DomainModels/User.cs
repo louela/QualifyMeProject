00afe22591766a0aa28d5cdcac2e1a87a141a77c
Lorenelle <lorencisneros00@gmail.com>
2022-06-03 15:28:27 +0800

    Auto stash before merge of "lors" and "origin/louela"

---
+++ QualifyMeProject.DomainModels/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QualifyMeProject.DomainModels
+{
+    internal class User
+    {
+    }
+}
