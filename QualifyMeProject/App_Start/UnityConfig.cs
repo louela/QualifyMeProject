f2bbe6717a39fb3ee76e1bd7f9c21a589e7b40d4
Louela <louelabalili15@gmail.com>
2022-06-05 13:29:54 +0800

    Depndcy Injection and Register View Page

---
+++ QualifyMeProject/App_Start/UnityConfig.cs
@@ -0,0 +1,27 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using Unity;
+using Unity.WebApi;
+using Unity.Mvc5;
+using QualifyMeProject.ServiceLayer;
+
+namespace QualifyMeProject
+{
+
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            container.RegisterType<IUsersService, UsersService>();
+            DependencyResolver.SetResolver(new Unity.Mvc5.UnityDependencyResolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new Unity.WebApi.UnityDependencyResolver(container);
+
+
+        }
+    }
+}
